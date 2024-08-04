45900062eac2c3aceb0723387a8bfb288792947c
Ritwiz <madangopalsingh2803@gmail.com>
2024-08-04 23:52:46 +0530

    Showing the analysis of the image on the information screen

--- lib/screens/information_screen.dart
+++ lib/screens/information_screen.dart
@@ -1,18 +1,48 @@
-// ignore_for_file: use_key_in_widget_constructors, use_super_parameters
+// ignore_for_file: use_key_in_widget_constructors, use_super_parameters, prefer_const_constructors_in_immutables, prefer_const_constructors
 
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 
 class InformationScreen extends StatelessWidget {
+  final Uint8List imageBytes;
+  final String analysisResult;
 
-   final Uint8List imageBytes;
-   final String analysisResult;
-
-  const InformationScreen({Key? key,required this.imageBytes,required this.analysisResult,}):super(key: key);
+  InformationScreen({
+    Key? key,
+    required this.imageBytes,
+    required this.analysisResult,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pet Analysis'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.memory(
+              imageBytes,
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              analysisResult.isEmpty
+                  ? 'No animal found in the image.'
+                  : analysisResult,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
